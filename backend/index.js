1b55d666227b7674bfc3b230f1193039837fe4cc
Anika Malhotra <you@example.com>
2025-03-31 12:21:51 +0530

    project complete

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./src/lib/db.js";
 
 import authRoutes from "./src/routes/auth.route.js";
@@ -14,6 +16,8 @@ dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
@@ -25,6 +29,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT)
